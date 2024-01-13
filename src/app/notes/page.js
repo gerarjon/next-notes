78b562944da62aceb1978620b2b1fbbf128f1909
gerarjon <gerarsuaverdez@gmail.com>
2024-01-12 17:10:14 -0800

    further adjusted layout of page/nav/note

--- src/app/notes/page.js
+++ src/app/notes/page.js
@@ -10,9 +10,9 @@ export default async function NotesPage() {
   const notes = await getNotes();
 
   return(
-    <div className='w-5/6 mx-auto'>
-      <h1>Notes</h1>
-      <div className='md:grid md:grid-cols-4 gap-4'>
+    <div>
+      <h1 className='text-2xl font-bold underline underline-offset-8 decoration-wavy my-5'>Notes</h1>
+      <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4'>
         {notes?.map((note) => {
           return <Note key={note.id} note={note} />
         })}
@@ -26,9 +26,9 @@ function Note({note}) {
   return (
     <Link href='/notes/${id}' className='rounded shadow-md overflow-hidden bg-yellow-200 hover:bg-yellow-100'>
       <div className='px-6 py-4'>
-        <h2 className='font-bold text-xl mb-2'>{title}</h2>
+        <h2 className='font-semibold text-lg mb-2'>{title}</h2>
         <p className='text-gray-700 text-base mb-2'>{content}</p>
-        <p className='text-xs text-gray-700'>{created}</p>
+        <p className='text-gray-700 text-xs '>{created}</p>
       </div>
     </Link>
   ) 
